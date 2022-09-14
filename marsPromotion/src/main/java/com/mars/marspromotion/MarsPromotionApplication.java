745f42d869e74646c7161278d6f1fadcbfd578f1
dbsdmsdud812 <dbsdmsdud912@gmail.com>
2022-09-14 13:08:21 +0900

    first upload

---
+++ marsPromotion/src/main/java/com/mars/marspromotion/MarsPromotionApplication.java
@@ -0,0 +1,13 @@
+package com.mars.marspromotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsPromotionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsPromotionApplication.class, args);
+    }
+
+}
---
+++ marsPromotion/src/test/java/com/mars/marspromotion/MarsPromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.marspromotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsPromotionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
